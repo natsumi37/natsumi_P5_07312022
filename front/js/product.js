b004d9c01e20bbfae5a06448a5e66b2fb40cb9e9
natsumi37 <natsumi-sakai@eagle.sophia.ac.jp>
2022-10-25 00:14:44 +0900

    adding semicolons and checking duplicate items

--- front/js/product.js
+++ front/js/product.js
@@ -4,119 +4,102 @@
 
 // Get the id of an item selected
 
-const searchParam = new URLSearchParams(window.location.search)
-const itemId = searchParam.get("id")
+const searchParam = new URLSearchParams(window.location.search);
+const itemId = searchParam.get("id");
 
 
-// Fetch all data of the item by using the id
+// Fetch data of the item by using the id
 
-const url = "http://localhost:3000/api/products/"
+const url = "http://localhost:3000/api/products/";
 
 async function fetchItem() {
   try {
-    const response = await fetch(url + itemId)
-    const data = await response.json()
-    console.log(data)
+    const response = await fetch(url + itemId);
+    const data = await response.json();
+    console.log(data);
 
-    const itemImg = document.querySelector(".item__img")
-    const itemName = document.getElementById("title")
-    const itemPrice = document.getElementById("price")
-    const itemDesc = document.getElementById("description")
-    const itemColor = document.getElementById("colors")
+    const itemImg = document.querySelector(".item__img");
+    const itemName = document.getElementById("title");
+    const itemPrice = document.getElementById("price");
+    const itemDesc = document.getElementById("description");
+    const itemColor = document.getElementById("colors");
     
     itemImg.innerHTML  = `
     <img src="${data.imageUrl}" alt="${data.altTxt}">
-    `
+    `;
     itemName.innerHTML = `
     <h1>${data.name}</h1>
-    `
+    `;
     itemPrice.innerHTML = `
     <span>${data.price}</span>
-    `
+    `;
     itemDesc.innerHTML = `
     <p>${data.description}</p>
-    `
+    `;
 
     for (let i = 0; i < data.colors.length; i++) {
       itemColor.innerHTML += `
       <option value="${data.colors[i]}">${data.colors[i]}</option>
-      `
+      `;
     }
   } catch(error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
-fetchItem()
+fetchItem();
 
 
-// Add items to a cart by local storage
+// Add items to a cart and local storage
 
-const selectColor = document.getElementById("colors")
-const selectQuantity = document.getElementById("quantity")
-const addToCartButton = document.getElementById("addToCart")
+const selectColor = document.getElementById("colors");
+const selectQuantity = document.getElementById("quantity");
+const addToCartButton = document.getElementById("addToCart");
 
-let cartItems = [] // item id, quantity, color
+let cartItems = [];
 
-addToCartButton.addEventListener("click", function() {
+addToCartButton.addEventListener("click", (event) => {
+  event.preventDefault();
 
   if (selectQuantity.value > 0 && selectQuantity.value < 101) {
 
-    if (localStorage.getItem("storeCart") === null) {
+    if (localStorage.getItem("cart") === null) {
 
       const selectedItem = {
         _id: itemId,
         quantity: parseInt(selectQuantity.value),
         color: selectColor.value
-      }
-      cartItems.push(selectedItem)
-      localStorage.setItem("storeCart", JSON.stringify(cartItems))
-      console.log(cartItems)
+      };
+      cartItems.push(selectedItem);
+      localStorage.setItem("cart", JSON.stringify(cartItems));
+      console.log(cartItems);
 
     } else {
-      cartItems = JSON.parse(localStorage.getItem("storeCart"))
+      cartItems = JSON.parse(localStorage.getItem("cart"));
       
       const selectedItem = {
         _id: itemId,
         quantity: parseInt(selectQuantity.value),
         color: selectColor.value
-      }
+      };
+      let duplicateItems = false;
 
       for (let i = 0; i < cartItems.length; i++) {
         if (cartItems[i]._id === selectedItem._id && cartItems[i].color === selectedItem.color) {
-          cartItems[i].quantity += selectedItem.quantity
-        } else {
-          cartItems.push(selectedItem)
-          localStorage.setItem("storeCart", JSON.stringify(cartItems))
-        }
+          cartItems[i].quantity += selectedItem.quantity;
+          duplicateItems = true;
+          break;
+        } 
       }
-      console.log(cartItems)
+      if (!duplicateItems) {
+        cartItems.push(selectedItem);
+      }
+
+      localStorage.setItem("cart", JSON.stringify(cartItems));
+      console.log(cartItems);
     }
   } else {
-    alert("Please choose the item count between 1-100")
+    alert("Please choose the item count between 1-100");
   }
 
 })
-
-
-
-
-// function pushItemToCart() {
-
-//   let selectedItem = {
-//     _id: itemId,
-//     quantity: parseInt(selectQuantity.value),
-//     color: selectColor.value
-//   }
-
-//   for (let i = 0; cartItems.length; i++) {
-//     if (cartItems[i]._id === selectedItem._id && cartItems[i].color === selectedItem.color) {
-//       cartItems[i].quantity += selectedItem.quantity
-//     } else {
-//       cartItems.push(selectedItem)
-//     }
-//   }
-//   localStorage.setItem("storeCart", JSON.stringify(cartItems))
-// }
-
-
