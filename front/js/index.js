5f85a84101d8bb87441c0febe8b56bd5ad80881f
natsumi37 <natsumi-sakai@eagle.sophia.ac.jp>
2022-10-25 00:11:55 +0900

    adding semicolons

--- front/js/index.js
+++ front/js/index.js
@@ -2,19 +2,18 @@
  * INSERTIG ALL THE PRODUCT INFORMATION FROM THE API INTO THE WEBSITE
  */
 
-
-const url = "http://localhost:3000/api/products/"
-
 // Fetch data of all products
+
+const url = "http://localhost:3000/api/products/";
 
 async function fetchItems() {
   try {
-    const response = await fetch(url)
-    const data = await response.json()
+    const response = await fetch(url);
+    const data = await response.json();
 
     for (let i = 0; i < data.length; i++) {
 
-      const items = document.getElementById("items")
+      const items = document.getElementById("items");
 
       items.innerHTML += `
         <a href="./product.html?id=${data[i]._id}">
@@ -24,12 +23,12 @@ async function fetchItems() {
             <p>${data[i].description}</p>
           </article>
         </a>
-      `
+      `;
     }
   } catch(error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
-fetchItems()
+fetchItems();
 
